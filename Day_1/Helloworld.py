8912f7181294c5f8ac0ba1a8d13d4c65b9fb4881
YourUsernam20git config --global user.email <20adamabbas@gmail.com>
2023-12-04 00:53:00 +0100

    Create Helloworld.py

---
+++ Day_1/Helloworld.py
@@ -0,0 +1,72 @@
+# Day One of Arewa Data Science Python Challenge Exercises
+# Exercise: Level 0ne
+## 1. Check the python version you are using
+!python --version
+## 2. Open the python interactive shell and do the following operations. The operands are 3 and 4.
+##### Addition
+print(3+4)
+##### Subtraction
+print(3-4)
+##### Multiplication
+print(3*4)
+##### Division
+print(3/4)
+##### Exponentiation
+print(3**4)
+##### Modulos
+print(3 % 4)
+##### Floor division
+print(3//4)
+## 3. Write strings on the python interactive shell. The strings are the following:
+##### My Name: 'Adam Abbas Abba'
+print('Adam Abbas Abba')
+##### My Family Name: 'Abba'
+print('Abba')
+##### My Country: 'Nigeria'
+print('Nigeria')
+##### I am enjoying 30 days of python
+print('I am enjoying 30 days of python')
+## 4. Check the data types of the following data:
+##### 10
+print(type(10))
+##### 9.8
+print(type(9.8))
+##### 3.14
+print(type(3.14))
+##### 4-4j
+print(type(4-4j))
+##### Asabeneh, Python, Finland
+print(type(['Asabeneh', 'Python', 'Finland']))
+##### My Name
+print(type('Adam Abbas Abba'))
+##### My Family
+print(type('Abba'))
+##### My Country
+print(type('Nigeria'))
+# Level 3 exercise
+## 1. Examples of Data Type
+### Number;  Integer, Float, Complex
+integer = 1
+float =  1.5 
+complex = 2 + 8j
+print(integer)
+print(float)
+print(complex)
+### String
+a = "AREWA"
+print(a)
+### Boolean
+a = True
+print(a)
+### List
+a = ['Khalipha',9,1.5,True,False,['DataScientist']] 
+print(a)
+### Tuples
+a = ("Me","Good")
+print(a)
+### Set
+a = {1,3,4,5}
+print(a)
+### Dictionary
+a = {'Python':3,'Today':1,'Year':2023}
+print(a)
